7354506effc1bfb88f1c4a9ca5a70ad002c15b60
bhu1-103 <103.bhuvan@gmail.com>
2024-12-26 23:58:43 +0530

    Thu Dec 26 11:58:43 PM IST 2024 -> script fix

--- mouse.sh
+++ mouse.sh
@@ -6,6 +6,8 @@ xdotool click --repeat 3 --delay 200 1
 xdotool key ctrl+c
 xdotool key super+1
 xdotool type './query.sh "'
+sleep 1
+xdotool key super+2
 xdotool key ctrl+shift+v
 xdotool type '"'
 xdotool key return
