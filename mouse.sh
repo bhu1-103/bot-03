5f92040bd5c65d151fa5ccc5acd31bbf68284e17
bhu1-103 <103.bhuvan@gmail.com>
2024-12-27 01:15:55 +0530

    Fri Dec 27 01:15:55 AM IST 2024 -> perfect

--- mouse.sh
+++ mouse.sh
@@ -2,15 +2,14 @@
 
 #get
 xdotool key super+2
-xdotool mousemove 1100 1250
+xdotool mousemove 1050 1250
 xdotool click --repeat 3 --delay 200 1
 xdotool key ctrl+c
 xdotool key super+1
-xdotool type './query.sh "'
+xdotool type './query.sh "reply asap: '
 xdotool key ctrl+shift+v
 xdotool type '"'
 xdotool key return
-cat answer.txt | xclip -selection clipboard
 
 #wait
 while [[ ! -s answer.txt ]]; do
@@ -18,6 +17,7 @@ while [[ ! -s answer.txt ]]; do
 done
 
 #send
+cat answer.txt | xclip -selection clipboard
 xdotool key super+2
 xdotool key ctrl+v
 xdotool key return
