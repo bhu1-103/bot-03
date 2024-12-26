a5ec673c6874a2cca4150de35442b9a8ed24c713
bhu1-103 <103.bhuvan@gmail.com>
2024-12-27 00:09:14 +0530

    Fri Dec 27 12:09:14 AM IST 2024 -> gitignore

--- mouse.sh
+++ mouse.sh
@@ -1,13 +1,19 @@
 #!/usr/bin/zsh
 
+#get
+rm answer.txt
 xdotool key super+2
-xdotool mousemove 1300 1250
+xdotool mousemove 1100 1250
 xdotool click --repeat 3 --delay 200 1
 xdotool key ctrl+c
 xdotool key super+1
 xdotool type './query.sh "'
-sleep 1
-xdotool key super+2
 xdotool key ctrl+shift+v
 xdotool type '"'
+xdotool key return
+cat answer.txt | xclip -selection clipboard
+
+#send
+xdotool key super+2
+xdotool key ctrl+v
 xdotool key return
